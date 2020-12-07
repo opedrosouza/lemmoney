2b1dae54f470092d1a194a27a5758ce94695f1a2
= <=>
2020-12-07 12:51:29 -0300

    Trying to fix heroku relation problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '/',
-  path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'register',
-    sign_up: 'new'
-  },
-  controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    confirmations: 'users/confirmations'
-  }
-
-  devise_scope :user do
-    unauthenticated :user do
-      root 'pages#home', as: :unauthenticated_root
-    end
-    authenticated do
-      root 'pages#home', as: :authenticated_root
-    end
-  end
+  
 
   resources :offers, only: [:index]
 
